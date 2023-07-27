cc5fef7e1f6f6bd6d1b80900460d7b658f3197a9
rociohleija <137197779+rociohleija@users.noreply.github.com>
2023-07-27 12:43:40 -0500

    Change the API Key coordinates

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ function displayForecast() {
 }
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "82d232f689d92fca314b1eb07a4d627c"; 
+  let apiKey = "5201594abea9f3e38b70e65b11a80c24"; 
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
